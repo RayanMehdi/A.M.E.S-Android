35d158a9fd10d15147f9768159ee3b7195ee39cd
Thomas <thomas.chaboud@etu.univ-lyon1.fr>
2018-01-17 15:56:06 +0100

    [TASK] ajout page accueil

--- app/src/main/java/com/example/iem/ames/MainActivity.java
+++ app/src/main/java/com/example/iem/ames/MainActivity.java
@@ -1,15 +1,17 @@
 package com.example.iem.ames;
 
+import android.content.pm.ActivityInfo;
+import android.graphics.Point;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Display;
+import android.view.Gravity;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import com.example.iem.ames.model.AMESGame;
 import com.example.iem.ames.model.AMESSequence;
 import com.example.iem.ames.parser.AMESParser;
@@ -19,45 +21,40 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        // Remember that you should never show the action bar if the
+        // status bar is hidden, so hide that too if necessary.
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null){
+            actionBar.hide();
+        }
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int width = size.x;
+        int height = size.y;
+
+        setContentView(R.layout.activity_main);
+        RelativeLayout cl = findViewById(R.id.layout);
+
+        TextView tv = new TextView(this);
+        tv.setHeight(height);
+        tv.setWidth(width);
+        tv.setTextSize(20);
+        tv.setText(R.string.loadingTextLabel);
+        tv.setTextColor(getResources().getColor(R.color.white));
+        tv.setGravity(Gravity.CENTER);
+        cl.addView(tv);
 
         AMESParser parser = new AMESParser();
         AMESGame amesGame = new AMESGame();
         AMESSequence testSequence = new AMESSequence(amesGame);
         parser.AMESParser(this, testSequence);
         Log.d("TEST",testSequence.toString());
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
